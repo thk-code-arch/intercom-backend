0488176ff22a0bb1dcfa8fbb2420eb7fd64e8c2a
Steffen Stein <therealstein@tutanota.com>
2020-08-04 10:24:16 +0200

    JWT for sockets.io

--- io/index.js
+++ io/index.js
@@ -1,7 +1,5 @@
-
 const app = require('express')();
 const server = require('http').createServer(app);
-
 const io = require("socket.io")(server, {
     handlePreflightRequest: (req, res) => {
         const headers = {
@@ -13,13 +11,22 @@ const io = require("socket.io")(server, {
         res.end();
     }
 });
-io.on('connection', function(socket) {
+const socketioJwt = require('socketio-jwt');
+
+io.use(socketioJwt.authorize({
+  secret: process.env.IC_Secret,
+  handshake: true
+}));
+
+io.on('connection', (socket) => {
     console.log(socket.id)
     socket.on('SEND_MESSAGE', function(data) {
+    data.time = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    data.userid = socket.decoded_token.id;
     console.log(data);
        io.emit('MESSAGE', data);
     });
 });
-
-
+//TODO implement second rountrip to improve security
+//TODO implement jwt expire check
 server.listen(3000);
