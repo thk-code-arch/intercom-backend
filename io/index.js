53bbb423a6d656493ba1489dc46c585206ab92a1
Steffen Stein <therealstein@tutanota.com>
2020-08-10 18:02:23 +0200

    added namespaces !

--- io/index.js
+++ io/index.js
@@ -24,6 +24,8 @@ var authorizer = socketioJwt.authorize({
 });
 io.use(authorizer);
 io.of('/viewport').use(authorizer);
+io.of('/chatroom').use(authorizer);
+
 
 // TODO add Namespaces with Rooms inside. One Namespace for service. eg viewport,
 // Chatrooms, Notificatios x
@@ -34,69 +36,98 @@ let players = [];
 for (var i = 0; i <= 100; ++i) {
     players[i] =[] ;
 }
-// io.nsps[yourNamespace]sss.adapter.rooms[roomName]
-io.of("/viewport").on("connect", socket => {
-  socket.on("new-player", (player, ack) => {
-    player.sID = socket.decoded_token.id;
+
+//new NSP
+//sjjss
+
+
+var viewport = io.of("/viewport");
+viewport.on('connection', function(socket) {
+  console.log("Connect NSP/viewport UserID:",socket.decoded_token.id)//init
+  socket.on('newplayer', (player)=>{
     User.findOne({
         where: {
           id: socket.decoded_token.id
         }
       })
       .then(user => {
-        socket.join(player.chatroom);
-        console.log(socket);
-        player.username = user.username;
-        player.profile_image = user.profile_image;
-        players[player.chatroom].push(player);
-        io.of("/viewport").in(player.chatroom).emit("list-players", players[player.chatroom]);
-        ack(player);
       });
   });
-
-  socket.on("move-to", pos => {
-    players[pos.chatroom.toString()] = players[pos.chatroom.toString()].map(obj =>
-    obj.sID === socket.decoded_token.id ? { ...obj, camPos: pos.position } : obj );
-    io.of("/viewport").in(pos.chatroom).emit("list-players", players[pos.chatroom]);
-    // TODO remove duplicate player by sID, keep last !!!
+  socket.on('getInfo', function(msg){
+    socket.emit("info","response for printer.getInfo("+msg+")");
   });
 
-  socket.on("bye-bye", player => {
-    players[player.chatroom] = players[player.chatroom].filter(e => e.sID !== socket.decoded_token.id);
-    io.of("/viewport").in(player.chatroom).emit("list-players", players[player.chatroom]);
+  socket.on('disconnect', function(){
+    //nsp.emit("chat message", name+": disconnection");
   });
 });
 
 
-
-
-io.on('connection', (socket) => {
-    console.log(socket.id)
-    socket.on('SEND_MESSAGE', function(data) {
+var chatroom = io.of("/chatroom");
+chatroom.on('connection', (socket) => {
+    console.log("Connect NSP/chatroom UserID:",socket.decoded_token.id) //init
+    socket.on('send_message', function(data) {
       data.userid = socket.decoded_token.id;
       data.chatroomId = 1,
       data.time = new Date().toISOString().slice(0, 19).replace('T', ' ');
+      console.log(data);
       Chatlog.create(data);
       User.findOne({
         where: {
           id: socket.decoded_token.id
         }
       })
-        .then(user => {
-          console.log("Userid found");
-          data.username = user.username;
-          //TODO Profile url from ENV Url
-          data.profile_image = user.profile_image;
-          console.log(data);
-          io.emit('MESSAGE', data);
-        });
-    });
-    socket.on('camPos', function(data) {
-      data.userid = socket.decoded_token.id;
-      data.chatroomId = 1,
-      io.emit('usercamPos', data);
+      .then(user => {
+        console.log("Userid found");
+        data.username = user.username;
+        data.profile_image = user.profile_image;
+        console.log(data);
+        chatroom.emit('message', data);
+      });
     });
 });
+
+// ssjjjdelete this namespace
+// io.nsps[yourNamespace]sss.adapter.rooms[roomName]
+//io.of("/deletethis").on("connect", socket => {
+//  socket.on("new-player", (player, ack) => {
+//    player.sID = socket.decoded_token.id;
+//    User.findOne({
+//        where: {
+//          id: socket.decoded_token.id
+//        }
+//      })
+//      .then(user => {
+//        socket.join(player.chatroom);
+//        console.log(socket);
+//        player.username = user.username;
+//        player.profile_image = user.profile_image;
+//        players[player.chatroom].push(player);
+//        io.of("/viewport").in(player.chatroom).emit("list-players", players[player.chatroom]);
+//        ack(player);
+//      });
+//  });
+//
+//  socket.on("move-to", pos => {
+//    players[pos.chatroom.toString()] = players[pos.chatroom.toString()].map(obj =>
+//    obj.sID === socket.decoded_token.id ? { ...obj, camPos: pos.position } : obj );
+//    io.of("/viewport").in(pos.chatroom).emit("list-players", players[pos.chatroom]);
+//    // TODO remove duplicate player by sID, keep last !!!
+//  });
+//
+//  socket.on("bye-bye", player => {
+//    players[player.chatroom] = players[player.chatroom].filter(e => e.sID !== socket.decoded_token.id);
+//    io.of("/viewport").in(player.chatroom).emit("list-players", players[player.chatroom]);
+//  });
+//});
+
+//
+
+
 // TODO implement second rountrip to improve security
 // TODO implement jwt expire check
-server.listen(3000);
+
+server.listen(3000, function() {
+   console.log('listening on localhost:3000');
+});
+
