bf13ffc861cd7a11922637bd9cb11d2446db1aa9
Steffen Stein <therealstein@tutanota.com>
2020-12-15 23:26:48 +0100

    added static paths

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+  app.useStaticAssets('/files', {prefix: '/files'});
+  app.useStaticAssets('/files/static', {prefix: '/static'});
   await app.listen(3000);
 }
 bootstrap();
