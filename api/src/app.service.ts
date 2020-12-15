c8db96b573c930f9bcf42b55969f3f003d9984d9
Steffen Stein <therealstein@tutanota.com>
2020-12-15 16:32:42 +0100

    db module added

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '111122222Hello World!';
+    return 'hell111122222Hello World!';
   }
 }
--- api/src/db/db.module.ts
+++ api/src/db/db.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.IC_DBHost,
+      port: 3306,
+      username: 'root',
+      password: process.env.IC_DBPassword,
+      database: process.env.IC_Database,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
 export class DbModule {}
