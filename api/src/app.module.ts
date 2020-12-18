dbf223d4a504ac1ef365c080ec030271a899ef6b
Steffen Stein <therealstein@tutanota.com>
2020-12-18 23:21:31 +0100

    add migration

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { DbModule } from './database/db.module';
 import { AuthModule } from './auth/auth.module';
 import { ApiModule } from './api/api.module';
+import { configService } from './config/config.service';
 
 @Module({
   imports: [DbModule, AuthModule, ApiModule],
