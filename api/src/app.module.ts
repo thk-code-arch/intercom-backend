e7124aef10809c8cda50a1c6579d780c8b0e9392
Steffen Stein <therealstein@tutanota.com>
2020-12-20 18:11:05 +0100

    added static serving

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,14 @@ import { DbModule } from './database/db.module';
 import { AuthModule } from './auth/auth.module';
 import { ApiModule } from './api/api.module';
 import { ioModule } from './io/io.module';
+import { ServeStaticModule} from '@nestjs/serve-static';
 
 @Module({
-  imports: [DbModule, AuthModule, ApiModule, ioModule],
+  imports: [
+    ServeStaticModule.forRoot({
+    rootPath: ('/intercom-frontend/dist'),
+    }),
+    DbModule, AuthModule, ApiModule, ioModule],
   controllers: [],
   providers: [],
   exports: []
