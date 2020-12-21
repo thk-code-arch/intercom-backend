a26b5cd8ac4cef4d4d1c92ae3c10369de91645a7
Steffen Stein <therealstein@tutanota.com>
2020-12-21 20:37:44 +0100

    aadded modules

--- api/src/api/api.module.ts
+++ api/src/api/api.module.ts
@@ -3,9 +3,11 @@ import { ApiController } from './api.controller';
 import { UserModule } from './user/user.module';
 import { ProjectController } from './project/project.controller';
 import { ProjectModule } from './project/project.module';
+import { ChatModule } from './chat/chat.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   controllers: [ApiController, ProjectController],
-  imports: [UserModule, ProjectModule]
+  imports: [UserModule, ProjectModule, ChatModule, AdminModule]
 })
 export class ApiModule {}
