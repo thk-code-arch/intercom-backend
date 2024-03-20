64855e67f29253d0816744fbafb9cdecffe3b7e8
Steffen Stein <therealstein@tutanota.com>
2024-03-20 14:14:48 -0400

    updated backend dep

--- api/src/config/config.service.ts
+++ api/src/config/config.service.ts
@@ -4,9 +4,6 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { join } from 'path';
 
-// tslint:disable-next-line: no-var-requires
-require('dotenv').config();
-
 class ConfigService {
   constructor(private env: { [k: string]: string | undefined }) {}
 
